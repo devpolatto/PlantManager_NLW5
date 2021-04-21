fa854ceb2beb30d0437584018036eb58d9542787
devpolatto <polatto.xcode.job@gmail.com>
2021-04-20 23:59:25 -0300

    add TouchableWithoutFeedback

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -50,31 +50,33 @@ const UserIdentification = () => {
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <View style={styles.header}>
-                            <Text style={styles.emoji}>
-                                {isFilled ? '😄' : '😉'}
-                            </Text>
-                            <Text style={styles.title}>
-                                Como podemos {'\n'} chamar você?
-                            </Text>
-                            <TextInput
-                                style={[
-                                    styles.input,
-                                    (isFocused || isFilled) && { borderColor: colors.green },
-                                ]}
-                                placeholder="Digite seu nome"
-                                onBlur={handleInputBlur}
-                                onFocus={handleInputFocus}
-                                onChangeText={handleInputChange}
-                            />
-                            <View style={styles.footer}>
-                                <Button title="Confirmar" onPress={handleSubmit} />
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    {isFilled ? '😄' : '😉'}
+                                </Text>
+                                <Text style={styles.title}>
+                                    Como podemos {'\n'} chamar você?
+                                </Text>
+                                <TextInput
+                                    style={[
+                                        styles.input,
+                                        (isFocused || isFilled) && { borderColor: colors.green },
+                                    ]}
+                                    placeholder="Digite seu nome"
+                                    onBlur={handleInputBlur}
+                                    onFocus={handleInputFocus}
+                                    onChangeText={handleInputChange}
+                                />
+                                <View style={styles.footer}>
+                                    <Button title="Confirmar" onPress={handleSubmit} />
+                                </View>
                             </View>
                         </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     );

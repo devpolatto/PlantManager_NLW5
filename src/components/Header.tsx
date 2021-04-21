74277e09759db6f48127d6a6ef5c7eb003f483be
devpolatto <polatto.xcode.job@gmail.com>
2021-04-21 17:27:56 -0300

    class 3 completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, Image, Platform, StatusBar } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+const imgAvatar = "https://avatars.githubusercontent.com/u/51465694?v=4"
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.username}>Polatto</Text>
+            </View>
+
+            <Image source={{ uri: imgAvatar }} style={styles.image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    username: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
+    },
+});

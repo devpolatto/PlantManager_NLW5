a24f3043ccd0cd8af216d585110ed936bf778d99
devpolatto <polatto.xcode.job@gmail.com>
2021-04-25 17:56:31 -0300

    add notifications

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import * as Notifications from 'expo-notifications';
+
+import { PlantProps } from './src/libs/storage';
 
 import Routes from './src/routes/';
 
@@ -14,6 +17,27 @@ export default function App() {
     Jost_400Regular,
     Jost_600SemiBold
   })
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log('Nova Notificaçao \n\n\n', data)
+    //   }
+    // )
+
+    // return () => subscription.remove();
+
+    async function notifications() {
+      // await Notifications.cancelAllScheduledNotificationsAsync();
+
+      const data = await Notifications.getAllScheduledNotificationsAsync();
+      console.log('NOTIFICAÇÕES AGENDADAS #####')
+      console.log(data)
+    }
+
+    notifications()
+  }, [])
 
   if (!fonstLoaded) return <AppLoading />
 
